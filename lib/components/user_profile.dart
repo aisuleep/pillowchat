f6be7f21667be05890495f2e4d38065c576325c3
aisu <129549280+aisuleep@users.noreply.github.com>
2023-08-09 03:01:50 -0400

    fix: user profile padding

--- lib/components/user_profile.dart
+++ lib/components/user_profile.dart
@@ -337,52 +337,58 @@ class ProfileTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: ListView(
-        children: [
-          if (roles.length != 0)
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4),
-              child: Text(
-                'Roles'.toUpperCase(),
-                style: const TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ),
-
-          if (roles.length != 0)
-            RoleList(
-              roles: roles,
-            ),
-          // BIO
-
-          Text(
-            'Information'.toUpperCase(),
-            style: TextStyle(
-              color: Dark.secondaryForeground.value,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          if (user.profile?.content != null)
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                  color: Dark.primaryBackground.value,
-                  padding: const EdgeInsets.all(8),
-                  child: MarkdownBody(
-                    data: user.profile?.content ?? '',
-                    softLineBreak: true,
-                    styleSheet: markdown.styleSheet,
-                    builders: markdown.builders,
-                    extensionSet: markdown.extensionSet,
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (roles.length != 0)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: Text(
+                    'Roles'.toUpperCase(),
+                    style: const TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
+
+              if (roles.length != 0)
+                RoleList(
+                  roles: roles,
+                ),
+              // BIO
+
+              Text(
+                'Information'.toUpperCase(),
+                style: TextStyle(
+                  color: Dark.secondaryForeground.value,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-        ],
-      ),
+              if (user.profile?.content != null)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      color: Dark.primaryBackground.value,
+                      padding: const EdgeInsets.all(8),
+                      child: MarkdownBody(
+                        data: user.profile?.content ?? '',
+                        softLineBreak: true,
+                        styleSheet: markdown.styleSheet,
+                        builders: markdown.builders,
+                        extensionSet: markdown.extensionSet,
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
