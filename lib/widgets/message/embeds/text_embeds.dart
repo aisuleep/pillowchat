4a6717f406c23a028e14df277a6c8035c3614cf7
aisu <129549280+aisuleep@users.noreply.github.com>
2023-09-01 02:44:38 -0400

    refactor: text embed padding

--- lib/widgets/message/embeds/text_embeds.dart
+++ lib/widgets/message/embeds/text_embeds.dart
@@ -12,88 +12,81 @@ class TextEmbeds extends StatelessWidget {
   final dynamic index;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        top: 8,
-        bottom: 8,
-        right: 8,
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(8),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border(
-              left: BorderSide(
-                width: 4,
-                color: messageIndex.embeds[0].color != null &&
-                        messageIndex.embeds[0].color.contains('var') == false
-                    ? Color(
-                        int.parse(messageIndex.embeds[0].color
-                            .replaceAll('#', '0xff')),
-                      )
-                    : Dark.secondaryForeground.value.withOpacity(0.5),
-              ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            left: BorderSide(
+              width: 4,
+              color: messageIndex.embeds[0].color != null &&
+                      messageIndex.embeds[0].color.contains('var') == false
+                  ? Color(
+                      int.parse(
+                          messageIndex.embeds[0].color.replaceAll('#', '0xff')),
+                    )
+                  : Dark.secondaryForeground.value.withOpacity(0.5),
             ),
-            color: Dark.background.value,
           ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                    bottom: 4,
-                  ),
-                  child: Row(
-                    children: [
-                      if (messageIndex.embeds[0].iconUrl != null)
-                        Padding(
-                          padding: const EdgeInsets.only(right: 8),
-                          child: SizedBox(
-                            height: 20,
-                            width: 20,
-                            child: Image.network(
-                              messageIndex.embeds[0].iconUrl,
-                              fit: BoxFit.fill,
-                              filterQuality: FilterQuality.medium,
-                            ),
-                          ),
-                        ),
-                      if (messageIndex.embeds[0].title != null)
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(right: 8),
-                            child: Text(
-                              messageIndex.embeds[0].title,
-                              style: TextStyle(
-                                color: Dark.secondaryForeground.value,
-                              ),
-                            ),
-                          ),
-                        ),
-                    ],
-                  ),
+          color: Dark.background.value,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 4,
                 ),
-                if (messageIndex.embeds[0].description != null)
-                  MarkdownBody(
-                    softLineBreak: true,
-                    selectable: true,
-                    extensionSet: markdown.extensionSet,
-                    data: messageIndex.embeds[0].description,
-                    styleSheet: markdown.styleSheet,
-                    onTapLink: ((text, href, title) {
-                      Future<void> _launchUrl() async {
-                        if (!await launchUrl(Uri.parse(href!))) {
-                          throw Exception('Could not launch $href');
-                        }
+                child: Row(
+                  children: [
+                    if (messageIndex.embeds[0].iconUrl != null)
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8),
+                        child: SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: Image.network(
+                            messageIndex.embeds[0].iconUrl,
+                            fit: BoxFit.fill,
+                            filterQuality: FilterQuality.medium,
+                          ),
+                        ),
+                      ),
+                    if (messageIndex.embeds[0].title != null)
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 8),
+                          child: Text(
+                            messageIndex.embeds[0].title,
+                            style: TextStyle(
+                              color: Dark.secondaryForeground.value,
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+              if (messageIndex.embeds[0].description != null)
+                MarkdownBody(
+                  softLineBreak: true,
+                  selectable: true,
+                  extensionSet: markdown.extensionSet,
+                  data: messageIndex.embeds[0].description,
+                  styleSheet: markdown.styleSheet,
+                  onTapLink: ((text, href, title) {
+                    Future<void> _launchUrl() async {
+                      if (!await launchUrl(Uri.parse(href!))) {
+                        throw Exception('Could not launch $href');
                       }
+                    }
 
-                      _launchUrl();
-                    }),
-                  ),
-              ],
-            ),
+                    _launchUrl();
+                  }),
+                ),
+            ],
           ),
         ),
       ),
